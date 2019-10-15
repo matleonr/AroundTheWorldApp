4b0a5176b20eecb56a021751d9b98d563c0d41cc
Mateo Leon Restrepo <mateoleonrestrepo@iMac-de-Mateo.local>
2019-10-14 23:15:19 -0500

    settin country detail page and splash art

--- AroundTheWorld/AroundTheWorld.Prism/AroundTheWorld.Prism/ViewModels/CountryPageViewModel.cs
+++ AroundTheWorld/AroundTheWorld.Prism/AroundTheWorld.Prism/ViewModels/CountryPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using AroundTheWorld.Prism.Models;
+﻿using System.Collections.ObjectModel;
+using AroundTheWorld.Prism.Models;
 using Prism.Navigation;
 
 namespace AroundTheWorld.Prism.ViewModels
@@ -7,19 +8,43 @@ namespace AroundTheWorld.Prism.ViewModels
     {
         private readonly INavigationService _navigationService;
         private CountriesResponse _country;
+        private ObservableCollection<Language> _languages;
+        private Translations _translations;
 
         public CountryPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             _navigationService = navigationService;
-            Title = "Info of: ";
+            Title = "Country";
+        }
+
+        public CountriesResponse Country
+        {
+            get => _country;
+            set => SetProperty(ref _country, value);
+        }
+
+        public Translations Translations
+        {
+            get => _translations;
+            set => SetProperty(ref _translations, value);
+        }
+
+        public ObservableCollection<Language> Languages
+        {
+            get => _languages;
+            set => SetProperty(ref _languages, value);
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
         {
             if (parameters.ContainsKey("country"))
             {
-                _country = parameters.GetValue<CountriesResponse>("country");
-                Title = _country.Name;
+                Country = parameters.GetValue<CountriesResponse>("country");
+                Title = Country.Name;
+                //Languages = Country.Languages;
+                Translations = new Translations();
+                Translations = Country.Translations;
+                Languages = new ObservableCollection<Language>(Country.Languages);
             }
         }
     }
